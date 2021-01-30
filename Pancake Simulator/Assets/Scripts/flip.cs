de15ae83b526d2dcd55b3c75eab090894009ae3a
Cooper Grimes <grimescb@hendrix.edu>
2021-01-29 21:31:48 -0600

    Pancake and Pan
    The Pan can move with the move mouse movement script, but the Pancake flip script is commented out due to compile errors. For some reason I cannot see compile errors on visual studio.

---
+++ Pancake Simulator/Assets/Scripts/flip.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flip : MonoBehaviour
+{
+
+    //private Rigidbody2D rb2D;
+    //private float force = 0f;
+    //private float randforce = Random.Range(-250f, 250f)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         /* if (Input.GetButton("Fire1"))
+         {
+             force += 1;
+         }
+         else
+         {
+             rb2D = GetComponent<RigidBody2D>();
+             rb2D.AddForce(transform.up * force);
+             rb2d.AddForce(transform.right * randforce);
+         } */
+    }
+}
